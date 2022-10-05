ce7ee661d890655e7f2ed929a1d06b2eb44f1311
NoahWederking <114934855+NoahWederking@users.noreply.github.com>
2022-10-05 08:54:00 +0200

    console colors indsat

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,16 @@
 public class Main {
+    void run(){
+
+
+
+
+    }
+
+
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        new Main().run();
+
 
     }
 }
