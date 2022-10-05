62d13dc07760695970fbd65945d5c4af7ef763e5
Ingeman366 <oliv366x@stud.kea.dk>
2022-10-05 09:14:06 +0200

    Days

--- src/Day.java
+++ src/Day.java
@@ -1,4 +1,13 @@
 public class Day {
     private boolean holiday;
+    private Booking book1;
+    private Booking book2;
+    private Booking book3;
+    private Booking book4;
+    private Booking book5;
+    private Booking book6;
+    private Booking book7;
+    private DaysOfTheWeek daysOfTheWeek;
+
 
 }
